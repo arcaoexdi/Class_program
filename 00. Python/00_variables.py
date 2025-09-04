bd2f8d7eec5511fc84c7fb167920d3ab3c3643cb
arca_oexdi <arcaoexdi@gmail.com>
2025-09-04 08:52:41 -0500

    Agregamos carpeta de python para el aprendizaje del mismo

---
+++ 00. Python/00_variables.py
@@ -0,0 +1,7 @@
+# Realizaremos el estudio y aprendizaje de como se declaran y utilizan las variables en Python
+
+
+# Declaración de variables
+
+variable = int(input("Ingrese el numero para almacenarlo en la variable:"))
+
