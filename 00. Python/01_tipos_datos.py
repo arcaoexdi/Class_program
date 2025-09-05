3aabc74ec33b1e3050bf1c45437c15ae73e1880c
arca_oexdi <arcaoexdi@gmail.com>
2025-09-05 11:04:47 -0500

    Agregando cambios de python, segunda clases

--- 00. Python/01_tipos_datos.py
+++ 00. Python/01_tipos_datos.py
@@ -1,54 +1,112 @@
 # Tipos de datos en Python y conceptos claves
 
 # Tipos de datos basicos
-entero = int(10)          # Entero
-flotante = float(10.5)    # Flotante
-cadena = str("Hola")      # Cadena de texto
-booleano = bool(True)     # Booleano
-listas = list([1, 2, 3])  # Lista
-dictado = dict(a=1, b=2)  # Diccionario
-tupla = tuple((1, 2, 3))  # Tupla
-conjunto = set([1, 2, 3]) # Conjunto
+
+# Entero
+entero = int(11)
+print("Este es un dato entero ", entero)
+
+# Flotante
+flotante = float(105)    
+print("Este es un dato flotante ",flotante)
+
+# Cadena de texto
+cadena = str("Por ende es un String")
+print("Este es un dato tipo cadena, ",cadena)
+
+# Booleano
+booleano = bool(False)     
+print("Este es un dato tipo booleano, por ende, es verdadero o: ",booleano)
+
+# Lista
+listas = list([1, 2, 3])  
+print("Este es un dato tipo lista ",listas)
+
+# Diccionario
+dictado = dict(a=1, b=2)  
+print("Este es un dato tipo dictado, el cual da como resultado la siguiente expresion ",dictado)
+
+# Tupla
+tupla = tuple(('simpres', "dobles", "numero en comillas: 3", flotante, entero))  
+print("Este es un dato tipo tupla ", tupla)
+
+# Conjunto
+conjunto = set([1, entero, "Hola mundo"]) 
+print("Este es un dato tipo conjunto ",conjunto)
+
 
 # Operadores: Aritmeticos(+, -, *, /), logicos (and, or, not), de comparacion (==, !=, >, <)
 suma = 5 + 3
-resta = 5 - 3
+print("Este es el resultado de la suma ",suma)
+
+resta = 5 - 7
+print("Este es el resultado de la resta ",resta)
+
 multiplicacion = 5 * 3
+print("Este es el resultado de la multiplicacion ",multiplicacion)
+
 division = 5 / 3
+print("Este es el resultado de la division ",division)  
 
 ## Logicos
 Y = suma and resta
+print("Este es el resultado del operador AND ",Y)
+
 o = suma or resta
-NO = not suma    
+print("Este es el resultado del operador OR ",o)
+
+NO = not resta
+print("Este es el resultado del operador NOT ",NO)
 
 ## Comparacion
 igual = (suma == resta)
+print("Este es el resultado del operador de comparacion igual ",igual)
+
 diferente = (suma != resta)
+print("Este es el resultado del operador de comparacion diferente ",diferente)
+
 mayor = (suma > resta)
+print("Este es el resultado del operador de comparacion mayor ",mayor)
+
 menor = (suma < resta)
+print("Este es el resultado del operador de comparacion menor ",menor)
 
 ## Condicionales
 # IF - ELSE - ELIF
 if suma > resta:
-    resultado = "Suma es mayor"
+    resultado1 = "Suma es mayor"
+    print("Si es mayor, imprime esto", resultado1)
 elif suma < resta:
-    resultado = "Resta es mayor"
+    resultado2 = "Resta es mayor"
+    print("Si es menor, imprime esto", resultado2)
 else:
-    resultado = "Son iguales"
+    resultado3 = "Son iguales"
+    print("Si son iguales, imprime esto", resultado3)
     
 ## Bucles
 # FOR
 for i in range(5):
-    print(i)
+    print("Iteracion sobre el numero 5: ",i)
+
 # WHILE
 contador = 0
 while contador < 5:
-    print(contador)
+    print("Iterar sobre un contador que da como resultado: ", contador)
     contador += 1
+    if contador == 5:
+        print("Contador ha llegado a 5, saliendo del bucle")
+        break
 
 ## Funciones
 def saludar(nombre):
+    print("Saludando a:", nombre)
     return f"Hola, {nombre}"
+
+### Uso de la funcion Saludar
+saludo = saludar(input("Ingrese su nombre: "))
+print(saludo)
+
+
 
 def parametros(nombre="Mundo"):
     return f"Hola, {nombre}"
---
+++ 00. Python/README.md
@@ -0,0 +1,2 @@
+# Aprendizaje-Python
+Repositorio para el aprendizaje de lenguajes de programación con los cuales Arca Oexdi trabajara
